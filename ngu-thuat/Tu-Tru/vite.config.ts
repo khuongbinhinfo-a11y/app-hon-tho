be51a2878e055fc0dd26fb1232ec34fb918decd8
ungdungthongminh-info <khuongbinh.info@gmail.com>
2026-05-18 22:32:15 +0700

    a
    a

---
+++ ngu-thuat/Tu-Tru/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/apps/tutru/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+  esbuild: {
+    jsx: "automatic",
+  },
+});
